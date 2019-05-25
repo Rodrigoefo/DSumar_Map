337dc22ae0ea639de906d65d25d26c9cd24bfc2e
Rodrigo <fuentesosorio@fuentesosorio.com>
2019-05-25 18:49:47 -0400

    Uploading modifications in sumar_con_map.rb

--- sumar_con_map.rb
+++ sumar_con_map.rb
@@ -4,20 +4,13 @@ b=a.map{|x| x+1}
 c=a.map{|x| x.to_f}
 d=a.select{|x| x>5}
 e=a.inject(0){|suma, x| suma+=x}
+f=a.count{|x| x<5}
 
-contador=0
-n=a.count
-n.times do |x|
-  if a[x]<5
-    contador+=1
-  end
-end
 
-#f=a.inject(0){|contador, x| x<5? contador+=1 : contador}
 
 print "Arreglo original = #{a}\n"
 print "Con .MAP se suma 1 a cada elemento = #{b}\n"
 print "Con .MAP se convierte a float cada elemento del arreglo= #{c}\n"
 print "Con .SELECT se descartan todos los elementos menores a 5 = #{d}\n"
 print "Con .INJECT se suman todos los elementos del arreglo = #{e}\n"
-print "con .COUNT se cuentan los elementos menores de 4 = #{contador}\n"
+print "con .COUNT se cuentan los elementos menores de 5 = #{f}\n"
